23a681daeba26d9352642f767e07aa0afa291ba2
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-22 18:23:12 +0100

    feat: add solution

--- sumByFactors.ts
+++ sumByFactors.ts
@@ -21,33 +21,63 @@
 
 // Example: I = [15, 30, -45] 5 divides 15, 30 and (-45) so 5 appears in the result, the sum of the numbers for which 5 is a factor is 0 so we have [5, 0] in the result amongst others.
 
-const PRIME_INTEGERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41];
+// ========================
+
+function checkIfPrime(num: number): boolean {
+    num = Math.abs(num);
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
 
 function checkIfDivisibleBy(numerator: number, denominator: number): boolean {
     return numerator % denominator === 0;
 }
 
 function findPrimeFactors(num: number): number[] {
-    if (PRIME_INTEGERS.includes(Math.abs(num))) return [];
-    return PRIME_INTEGERS.filter((n) => checkIfDivisibleBy(num, n));
+    const factors: number[] = [];
+    const absNum = Math.abs(num);
+    const sqrtNum = Math.sqrt(absNum);
+
+    if (checkIfPrime(absNum)) factors.push(absNum);
+
+    for (let i = 2; i <= sqrtNum; i++) {
+        if (checkIfDivisibleBy(absNum, i)) {
+            if (checkIfPrime(i)) factors.push(i);
+            if (checkIfPrime(absNum / i) && absNum / i !== i) {
+                factors.push(absNum / i);
+            }
+        }
+    }
+    return factors;
 }
 
 function sumOfDivided(lst: number[]): number[][] {
-    const primeFactors = lst.map(findPrimeFactors);
+    // Flatten the array of arrays and convert to set
+    // to make unique list of prime factors
+    const primeFactors = new Set(lst.map(findPrimeFactors).flat().sort());
 
-    console.log(primeFactors);
+    let outputArr: number[][] = [];
 
-    // create a finalArr
-    // for each int, create a newArr like this [int, 0]
-    // for each num in list[]
-    // check if the num divides by the int
-    // if it does, increment the int at the second position by the value of the num
-    // push this newArr of two ints onto the finalArr
+    primeFactors.forEach((pf) => {
+        let pfArr: number[] = [pf, 0];
 
-    // sort the finalArr by the values at newArr[1] (the first of the two in each subarray) and return it
-    return [];
+        lst.forEach((num) => {
+            if (checkIfDivisibleBy(num, pf) === true) {
+                pfArr[1] += num;
+            }
+        });
+        outputArr.push(pfArr);
+    });
+    return outputArr.sort((a, b) => a[0] - b[0]);
 }
 
+// Ordinary test cases
 console.log(sumOfDivided([12, 15])); // [ [2, 12], [3, 27], [5, 15] ]
 console.log(sumOfDivided([15, 21, 24, 30, 45])); // [ [2, 54], [3, 135], [5, 90], [7, 21] ]
 
